0f5377953a2ea3e2b8892333611df49c227fda6a
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-13 16:57:31 +0100

    Just did some few random class stuff

--- Program.cs
+++ Program.cs
@@ -5,6 +5,3 @@
 //    TotalPrice = 100f
 //};
 //orderProcessor.Process(order);
-Random random = new Random(); 
-Console.WriteLine(random.Next(1, 10));
-Console. WriteLine("This console.writeline has a space in between it yet it does not have any error in running it");
