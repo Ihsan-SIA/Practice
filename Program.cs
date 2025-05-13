c8ae9c94ba13609e63e6d41b72a9bc9dada0870e
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-13 16:57:43 +0100

    Just did some few random class stuff

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿//var orderProcessor = new OrderProcessor(new ShippingCalculator());
-//var order = new Order
-//{
-//    DatePlaced = DateTime.Now,
-//    TotalPrice = 100f
-//};
-//orderProcessor.Process(order);
+﻿var orderProcessor = new OrderProcessor(new ShippingCalculator());
+var order = new Order
+{
+    DatePlaced = DateTime.Now,
+    TotalPrice = 100f
+};
+orderProcessor.Process(order);
